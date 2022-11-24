745d56b67367aef0b3b392bf6ef74e63f75e7be5
Root <Root.shelling@gmail.com>
2022-11-24 23:31:09 +0300

    chown

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 NAME=rspamd
 
+sudo chown -R _rspamd:_rspamd /etc/rspamd/*
 sudo /usr/bin/rspamadm configtest -c /etc/rspamd/$NAME.conf
 sudo /usr/bin/$NAME -f -u _rspamd -g _rspamd -p /run/rspamd/$NAME.pid -c /etc/rspamd/$NAME.conf
 exec "$@"
